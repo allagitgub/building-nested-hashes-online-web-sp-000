1c49345f6d882423687bea9e3aa726793bad55d2
Alla Kuzminova <alchik@gmail.com>
2020-09-23 23:51:14 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -16,12 +16,12 @@ def third_challenge
         },
       :hero_friends => [
         {
-        :name => "Benvolio"
-        :age => "17"
+        :name => "Benvolio",
+        :age => "17",
         :attitude => "worried"},
         {
-        :name => "Mercutio"
-        :age => "18"
+        :name => "Mercutio",
+        :age => "18",
         :attitude => "hot-headed"}
       ]
    },
@@ -40,12 +40,12 @@ def third_challenge
         :status => "alive"
         },
       :heroine_friends => [{
-      :name => "Steven"
-      :age => "30"
+      :name => "Steven",
+      :age => "30",
       :attitude => "confused"},
       {
-      :name => "Nurse"
-      :age => "44"
+      :name => "Nurse",
+      :age => "44",
       :attitude => "worried"}]
    }
   }
